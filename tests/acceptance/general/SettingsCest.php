335f3706beea297c8d70fac564c5d9082f4c42a1
Tim Carr <tim@n7studios.co.uk>
2024-11-26 17:16:06 +0800

    Deactivate MemberMouse Plugin after test pass

--- tests/acceptance/general/SettingsCest.php
+++ tests/acceptance/general/SettingsCest.php
@@ -310,6 +310,7 @@ class SettingsCest
 	 */
 	public function _passed(AcceptanceTester $I)
 	{
+		$I->deactivateThirdPartyPlugin($I, 'membermouse-platform');
 		$I->deactivateConvertKitPlugin($I);
 		$I->resetConvertKitPlugin($I);
 	}
