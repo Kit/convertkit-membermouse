8fb7ba546dc14b006d79125e94e57d2695cb8db0
Tim Carr <tim@n7studios.co.uk>
2024-11-26 17:04:40 +0800

    Change login method

--- tests/_support/Helper/Acceptance/ThirdPartyPlugin.php
+++ tests/_support/Helper/Acceptance/ThirdPartyPlugin.php
@@ -49,10 +49,6 @@ class ThirdPartyPlugin extends \Codeception\Module
 	public function deactivateThirdPartyPlugin($I, $name)
 	{
 		// Login as the Administrator.
-		$I->loginAsAdmin();
-
-		/*
-		// Login as the Administrator.
 		$I->amOnPage('wp-login.php');
         $I->waitForElement('#user_login', 10);
         $I->waitForElement('#user_pass', 10);
@@ -60,7 +56,9 @@ class ThirdPartyPlugin extends \Codeception\Module
         $I->fillField('#user_login', $_ENV['TEST_SITE_ADMIN_USERNAME']);
         $I->fillField('#user_pass', $_ENV['TEST_SITE_ADMIN_PASSWORD']);
         $I->click('#wp-submit');
-        */
+
+        // Wait for admin interface to load.
+        $I->waitForElementVisible('body.wp-admin');
 
 		// Go to the Plugins screen in the WordPress Administration interface.
 		$I->amOnPluginsPage();
